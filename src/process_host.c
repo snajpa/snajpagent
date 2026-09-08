e372b5ee36e48b1949c66951762ae0ad4cca9adf
Pavel Snajdr <snajpa@snajpa.net>
2026-09-08 09:53:44 +0200

    WIP: use native NetBSD waitpid WNOWAIT without waitid

--- src/process_host.c
+++ src/process_host.c
@@ -1856,8 +1856,8 @@ snag_child_exited(struct snag_child *child)
         errno = error;
     }
     return rc;
-#elif defined(__FreeBSD__)
-    /* FreeBSD supports polling without releasing child ownership. */
+#elif defined(__FreeBSD__) || defined(__NetBSD__)
+    /* BSD waitpid supports polling without releasing child ownership. */
     int status;
     pid_t pid = waitpid(child->pid, &status, WNOHANG | WNOWAIT);
     if (pid < 0 && errno == ECHILD)
